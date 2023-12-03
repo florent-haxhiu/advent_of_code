fe390595bf3085e746a7ac3f902a171a3a1970a2
florent-haxhiu <florenthaxhiu03@gmail.com>
2023-12-03 19:52:08 +0000

    Part two of Day One complete

--- 2023/1/main.py
+++ 2023/1/main.py
@@ -1,15 +1,23 @@
+import re 
+
 def read_file(file_name: str):
     with open(file_name, 'r') as f:
         file_data = [line.strip() for line in f.readlines()]
     return file_data
 
-file_data = read_file('part_one.txt')
+file_data = read_file('part_two.txt')
 nums_in_word = ['one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine']
 
 def find_calibration(file_data: list):
     total = 0
     for data in file_data:
-        nums = [char for char in data if char.isnumeric()]
+        nums = []
+        for i,c in enumerate(data):
+            if c.isdigit():
+                nums.append(c)
+            for d,val in enumerate(nums_in_word):
+                if data[i:].startswith(val):
+                    nums.append(str(d+1))
         if len(nums) == 1:
             total += int(str(nums[0]) + str(nums[0]))
         else:
