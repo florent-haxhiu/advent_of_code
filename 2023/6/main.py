9c85baefede4c3b6724265e56c3d33681e2d56fa
Florent Haxhiu <florenthaxhiu03@gmail.com>
2023-12-18 12:44:47 +0000

    Day 6 part two completed

--- 2023/6/main.py
+++ 2023/6/main.py
@@ -2,20 +2,18 @@ import re
 
 file = open('input.txt', 'r').readlines()
 
-times = re.findall(r'\d+', file[0])
-distances = re.findall(r'\d+', file[1])
+time = int(file[0].split(':')[1].replace(' ', ''))
+distance = int(file[1].split(':')[1].replace(' ', ''))
 
 total = 1
 
-for time, distance in zip(times, distances):
+win = 0
 
-    win = 0
+for x in range(0, time):
+    distance_travelled = (time - x) * x
+    if distance_travelled > distance:
+        win += 1
 
-    for x in range(0, int(time)):
-        distance_travelled = (int(time) - x) * x
-        if distance_travelled > int(distance):
-            win += 1
-
-    total *= win
+total *= win
 
 print(total)
